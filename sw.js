95fe1d05470b590124969f0d804cf046aa4dd7ee
LoubbyAI <omeraldemir38@gmail.com>
2026-03-02 01:22:41 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,19 @@
-const CACHE='insaat-v1';
-const ASSETS=['./', './İnşaat Match-3 v6.html'];
+// Cache'i her güncellemede sürüm numarasını artır
+const CACHE='insaat-v9';
 
 self.addEventListener('install',e=>{
-  e.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
+    self.skipWaiting();
+});
+
+self.addEventListener('activate',e=>{
+    e.waitUntil(
+          caches.keys().then(keys=>
+                  Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))
+                                 )
+        );
+    self.clients.claim();
 });
 
 self.addEventListener('fetch',e=>{
-  e.respondWith(
-    caches.match(e.request).then(r=>r||fetch(e.request))
-  );
+    e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
 });
